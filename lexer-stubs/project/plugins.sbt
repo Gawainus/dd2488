a5c5752005f97d4c8f1d34420416a86c8e493631
Yumen <gawainus97@gmail.com>
2016-03-07 23:27:49 +0100

    Test facility ready. Add more tests

--- lexer-stubs/project/plugins.sbt
+++ lexer-stubs/project/plugins.sbt
@@ -1,2 +1,1 @@
 addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "4.0.0")
-addSbtPlugin("com.artima.supersafe" % "sbtplugin" % "1.1.0-RC6")
--- lexer-stubs/src/test/scala/LexerSpec.scala
+++ lexer-stubs/src/test/scala/LexerSpec.scala
@@ -2,9 +2,63 @@ package slacc
 package lexertests
 
 import org.scalatest.FlatSpec
+import lexer._
+import utils._
+import scala.io.Source
+import java.io.File
 
 class LexerSpec extends FlatSpec {
-  "An empty file should return only EOF token" in {
+
+  def processOptions(args: Array[String]): Context = {
+
+    val reporter = new Reporter()
+    var ctx = Context(reporter = reporter)
+
+    def processOption(args: List[String]): Unit = args match {
+      case "--help" :: args =>
+        ctx = ctx.copy(doHelp = true)
+        processOption(args)
+
+      case "--tokens" :: args =>
+        ctx = ctx.copy(doTokens = true)
+        processOption(args)
+
+      case "-d" :: out :: args =>
+        ctx = ctx.copy(outDir = Some(new File(out)))
+        processOption(args)
+
+      case f :: args =>
+        ctx = ctx.copy(files = new File(f) :: ctx.files)
+        processOption(args)
+
+      case Nil =>
+    }
+
+    processOption(args.toList)
+
+    if (ctx.files.size != 1) {
+      reporter.fatal("Exactly one file expected, "+ctx.files.size+" file(s) given.")
+    }
+
+    ctx
+  }
+
+  "An empty file" should "return only EOF token" in {
+
+
+    val url = getClass.getResource("/sample_0.slac")
+    val pathStr = url.toString.substring(5)
+    println(pathStr)
+
+    val inputs: Array[String] = Array(pathStr)
+    val ctx = processOptions(inputs)
+
+    val iter = Lexer.run(ctx)(ctx.files.head)
+
+    while (iter.hasNext) {
+      val n = iter.next()
+      println(n+"("+n.line+":"+n.col+")")
+    }
     assert(0 == 0)
   }
 }
